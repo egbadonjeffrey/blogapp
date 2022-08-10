c61d8000581050d7097a096f3e3a0fac9dd08b3d
theScarlettCoder <egbadonjeffrey@gmail.com>
2022-08-10 07:12:25 +0100

    Button Component

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,29 @@
-const Button = () => {
+const Button = (props) => {
+  const buttonEnableDisable = !props.isDisabled
+    ? styles.buttonEnabled
+    : styles.buttonEnabled;
   return (
-    <button className={customClassName} onClick={props.handleClick}>
-      {" "}
-      {props.children}{" "}
+    <button
+      id={props.id}
+      className={`button ${buttonEnableDisable}`}
+      onClick={props.clickHandler}
+      type={props.type}
+      disabled={props.isDisabled}
+    >
+      {props.value}
     </button>
   );
+};
+
+const styles = {
+  buttonEnabled:
+    "block w-[7rem] my-0 mx-auto border border border-solid border-[primary] rounded-2xl",
+  buttonDisabled: "",
+};
+
+Button.defaultProps = {
+  type: "button",
+  disabled: false,
 };
 
 export default Button;
